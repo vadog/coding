da500affa9031081224f9b66e0c294882153cf7b
Mihir Kale <mkale@e1z2r3p2.42.us.org>
2020-02-15 15:29:34 -0800

    update

--- obsticle.py
+++ obsticle.py
@@ -12,6 +12,23 @@ y = 300
 
 isjump = False
 jumpcount = 10
+spikeclear = True
+spikes = []
+
+class sp1ke():
+    def __init__(self,x,y,width,height,color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height 
+        self.color =  color
+        self.vel = 5
+
+
+    def draw(self,win):
+        pygame.draw.rect(win, self.color, (self.x,self.y, self.width, self.height))
+
+
 
 
 while run:
@@ -22,7 +39,7 @@ while run:
 
     keys= pygame.key.get_pressed()
 
-    pygame.draw.rect(win,(0,0,255),(x,y,100,100))
+    pygame.draw.rect(win,(0,0,255),(x,y,50,50))
 
     if not isjump:     
         if keys[pygame.K_UP]:   
@@ -40,7 +57,21 @@ while run:
             isjump = False
             jumpcount = 10
 
+    for spike in spikes:
+        if spike.x > -50:
+            spike.x -= spike.vel
+        else:
+            spikes.pop(spikes.index(spike))
+            spikeclear = True
+
     
+    if spikeclear:
+        spikes.append(sp1ke ( 400,320,30,30,(0,0,255) ))
+        spikeclear = False
+
+
+    for spike in spikes:
+        spike.draw(win)
     pygame.display.update()
 
 
