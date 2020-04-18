1e831527adebee546082ec8c9bc42cbc668cb175
vadog <vadog2672@gmail.com>
2020-04-18 15:10:54 -0700

    updated

--- wordCounter.py
+++ wordCounter.py
@@ -1,54 +1,83 @@
-print ("please type name of txt file")
-fileName = input()
+import os
+import random
+import verbOrganizer
 
-theFile = open(fileName, "r")
-fileContent = theFile.read()
-theFile.close()
+   #initializing
+Dir = "spanishwords"
+round = 0
+points = 0
+rounds = 10
+AllWords = verbOrganizer.getData(Dir)
+verbData = AllWords[0]
+lenOFFile = len(verbData)
+#prints amount of verbs
+print("there are", lenOFFile, "words")
 
+while round < rounds:
+    # changing values every round
+    randomVerb = verbData[random.randrange(0, len(verbData))]
+    allVerbTenses = []
+    for i in range(2,5):
 
-x=0
-numWords=0
-z = False
-w = []
-currentWordCars= []
-count = 0
-
-validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXXYZ0123456789"
-
-while x < len(fileContent):
-   
-    if fileContent[x]  in validCharacters :
-        currentWordCars.append(fileContent[x])
-        if not z:
-            numWords +=1
-            z = True
-    else:
-        z = False
-        
-        p = ("".join(currentWordCars))  
-        if currentWordCars == []:
-            pass
-        else:
-            w.append(p)
-        currentWordCars=[]
-  
-    x += 1
-# deal with last words
-p = ("".join(currentWordCars))
-if currentWordCars == []:
-    pass
-else:   
-    w.append(p)
+        for tense in randomVerb[i]:
+             allVerbTenses.append(tense)
     
-fwords = (",".join(w))
+    randomMultipleChoices = (random.sample(allVerbTenses, k=5))
+    randomAnswer = randomMultipleChoices[random.randrange(0, len(randomMultipleChoices))]   
+    randomQuestion = randomVerb[0]
+    
 
-with open(fileName, 'r') as f:
-    for line in f:
-        count += 1
+    # formating question
+    for i in range (2,5):
+        if randomAnswer in randomVerb[i]:
+            p = i
+            if p == 2:
+                h = ("present tense")
+            if p == 3:
+                h = ("preterite tense")
+            if p == 4:
+                h = ("imperfect tense")
+    
+    for i in range (2,5):
+        for r in range(len(randomVerb[i])):
+            if randomAnswer == randomVerb[i][r]:
+                if r == 0:
+                    propernoun = "yo"
+                if r == 1:
+                    propernoun = "tu"
+                if r == 2:
+                    propernoun = "usted"
+                if r == 3:
+                    propernoun = "nosotros"
+                if r == 4:
+                    propernoun = "ustedes"
+    
+    
+    # printing question
+    print("What is", randomQuestion, "in", h,": ", propernoun)
 
-print ("words: ",numWords )
-print("Total number of lines is: ", count)
-print (fwords)
+    for i in range(len(randomMultipleChoices)):
+        print(i+1, randomMultipleChoices[i])
+    
+    
+    #checking answer
+    answer1 = input()
+    while answer1 not in "12345":
+        print("invalid input")
+        answer1 = input()
+    answer1 = int(answer1)
+    while randomMultipleChoices[answer1-1] != randomAnswer:
+        print("Wrong.")
+        break
+    else:
+        print("You got it right.")
+        points += 1
+    #adding round
+    round += 1
+#ending game
+if round == rounds:
+    print("you got", points, "correct")
+
 
 
 
